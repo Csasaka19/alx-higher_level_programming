3b3c81b19748ba185419f17a95a5ea2405de2cac
Clive Sasaka <clivesasaka@gmail.com>
2023-02-09 21:09:17 +0300

    Baby web-scrapping steps

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let filename = process.argv[2];
+const fs = require('fs');
+fs.readFile(filename, 'utf-8', function(err, data){
+if(err){
+    console.log(err);
+}
+else{
+    console.log(data);
+}
+});
