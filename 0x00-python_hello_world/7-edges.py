9a31f3e42e38a2f1dd456cc455a94c148280da3a
Clive Sasaka <clivesasaka@gmail.com>
2023-07-21 12:33:37 +0300

    Correction on python hello-world

--- 0x00-python_hello_world/7-edges.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-word = "Holberton"
-word_first_three = word[:3]
-word_last_two = word[7:]
-middle_word = word[1:-2]
-print(f"First three letters: {word_first_3}")
-print(f"Last two letters: {word_last_2}")
-print(f"Middle word: {middle_word})
--- 0x00-python_hello_world/8-concat_edges.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = str[32:57] + str[92:96] + str[0:6]
-print(str)
